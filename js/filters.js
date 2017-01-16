274452bccaeb26e59718eff4cde49b2cb864d3f0
kriizjones <kristoffer.s.karlsson@hotmail.com>
2017-01-16 18:12:58 +0100

    apply codes for components

---
+++ js/filters.js
@@ -0,0 +1,53 @@
+
+angular.module("demoApp").directive("sayHello", function () {
+    return {
+        scope: false,
+        link: function ($scope, element, attrs) {
+            $scope.message = attrs.message;
+        },
+        template: "<h1> {{[message, user.firstName, user.lastName].join(' ')}}!</h1>"
+    };
+});
+
+(function () {
+    angular.module("demoApp").directive("sayHello", function () {
+        return {
+            scope: false,
+            link: function ($scope, element, attrs) {
+                $scope.message = attrs.message;
+            },
+            template: "<h1> {{[message, user.firstName, user.lastName].join(' ')}}!</h1>"
+        };
+    });
+})();
+
+
+(function () {
+    'use strict';
+    angular
+        .module("demoApp")
+        .filter('sortByFirstName', function () {
+                return function (array) {
+                    function compare(a, b) {
+                        return a.firstName.localeCompare(b.firstName)
+                    }
+                    return array.sort(compare);
+                }
+            }
+        );
+})();
+
+(function () {
+    'use strict';
+    angular
+        .module("demoApp")
+        .controller("mainController", mainController);
+
+    function mainController($scope, userService) {
+        $scope.users = userService.users();
+        $scope.newUser = {}
+        $scope.addUser = function () {
+            userService.add($scope.newUser);
+        }
+    }
+})();
